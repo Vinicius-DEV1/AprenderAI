9d666bd25508c24f667158478476b363552ac90e
VINICIUS-DEV1 <vinicius.cl.dev@gmail.com>
2026-02-19 21:59:05 -0300

    feat(ui): add global error display and session feedback to app layout

--- debug_stats.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Models\UserTopicStat;
-
-$s = UserTopicStat::where('user_id', 4)->get();
-echo $s->toJson(JSON_PRETTY_PRINT);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -432,6 +432,27 @@
                         </div>
                     @endif
 
+                    {{-- Validation Errors Alert --}}
+                    @if ($errors->any())
+                        <div class="mb-6 bg-red-50 dark:bg-red-950/30 border-l-4 border-red-500 p-4 rounded-r shadow-sm">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <div class="ml-3">
+                                    <h3 class="text-sm font-medium text-red-800 dark:text-red-200">Encontramos alguns problemas:</h3>
+                                    <ul class="mt-2 list-disc list-inside text-sm text-red-700 dark:text-red-300">
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+
                     {{ $slot ?? '' }}
                     @yield('content')
                 </div>
